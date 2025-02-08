f2a96569cca0d6b1812fb60f98424b97c85bf867
Monjima Lahiri <123318569+MonjimaLahiri@users.noreply.github.com>
2025-02-07 19:29:20 -0800

    05-triangle.js

---
+++ 05-triangle.js
@@ -0,0 +1,8 @@
+// Declare an empty string to build the triangle
+let triangle = "";
+
+// Loop 7 times to create the triangle
+for (let i = 1; i <= 7; i++) {
+    triangle += "#"; // Add a "#" to the string in each iteration
+    console.log(triangle); // Print the current state of the triangle
+}
